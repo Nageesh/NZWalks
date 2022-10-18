458c08d87e47cea340b73e3d27f965f701d8355c
nageesh <gsnagesh99@gmail.com>
2022-10-18 17:48:08 -0400

    User,role and users_roles tables created in db

--- NZWalks/NZWalks.API/Migrations/20221018194951_Adding users.cs
+++ NZWalks/NZWalks.API/Migrations/20221018194951_Adding users.cs
@@ -9,24 +9,7 @@ namespace NZWalks.API.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Regions",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Area = table.Column<double>(type: "float", nullable: false),
-                    Lat = table.Column<double>(type: "float", nullable: false),
-                    Long = table.Column<double>(type: "float", nullable: false),
-                    Population = table.Column<long>(type: "bigint", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Regions", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
+          migrationBuilder.CreateTable(
                 name: "Roles",
                 columns: table => new
                 {
@@ -54,17 +37,7 @@ namespace NZWalks.API.Migrations
                     table.PrimaryKey("PK_Users", x => x.Id);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "WalkDifficulty",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_WalkDifficulty", x => x.Id);
-                });
+       
 
             migrationBuilder.CreateTable(
                 name: "Users_Roles",
@@ -91,32 +64,7 @@ namespace NZWalks.API.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Walks",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Length = table.Column<double>(type: "float", nullable: false),
-                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    WalkDifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Walks", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Walks_Regions_RegionId",
-                        column: x => x.RegionId,
-                        principalTable: "Regions",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Walks_WalkDifficulty_WalkDifficultyId",
-                        column: x => x.WalkDifficultyId,
-                        principalTable: "WalkDifficulty",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+    
 
             migrationBuilder.CreateIndex(
                 name: "IX_Users_Roles_RoleId",
@@ -128,24 +76,14 @@ namespace NZWalks.API.Migrations
                 table: "Users_Roles",
                 column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Walks_RegionId",
-                table: "Walks",
-                column: "RegionId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Walks_WalkDifficultyId",
-                table: "Walks",
-                column: "WalkDifficultyId");
+           
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Users_Roles");
-
-            migrationBuilder.DropTable(
-                name: "Walks");
+ 
 
             migrationBuilder.DropTable(
                 name: "Roles");
@@ -153,11 +91,7 @@ namespace NZWalks.API.Migrations
             migrationBuilder.DropTable(
                 name: "Users");
 
-            migrationBuilder.DropTable(
-                name: "Regions");
-
-            migrationBuilder.DropTable(
-                name: "WalkDifficulty");
+ 
         }
     }
 }
